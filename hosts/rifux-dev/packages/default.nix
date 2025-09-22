051022e0d7a64121e821754e35afe9befdfc7719
rifux 🌸 Vladimir Blinkov <contact@rifux.dev>
2025-09-22 03:05:08 +0300

    fix(hosts): Add CUPS service for Pantum drivers for `rifux-dev`
    Signed-off-by: rifux 🌸 Vladimir Blinkov <contact@rifux.dev>

--- hosts/rifux-dev/packages/default.nix
+++ hosts/rifux-dev/packages/default.nix
@@ -1,7 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {  
+  # Enable CUPS to print documents & select Pantum driver.
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.pantum-driver ]; 
   environment.systemPackages = with pkgs; [
-    pantum-driver # For printer
+    # 
   ];
 }
