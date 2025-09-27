84ddec75f781af92c8e1e3fb43bc56ba052fcedb
rifux 🌸 Vladimir Blinkov <contact@rifux.dev>
2025-09-27 14:00:32 +0300

    feat(system): Add proxied Komikku .desktop file
    Signed-off-by: rifux 🌸 Vladimir Blinkov <contact@rifux.dev>

--- packages/proxychains.nix
+++ packages/proxychains.nix
@@ -12,5 +12,18 @@
   environment.systemPackages = [
     # Install the package you need (proxychains or proxychains-ng)
     pkgs.proxychains-ng
+
+    # Add .desktop entries for proxy-enabled apps
+    (pkgs.makeDesktopItem {
+      name = "Komikku (Proxy)";
+      desktopName = "Komikku (Proxy)";
+      exec = "proxychains4 komikku";
+      icon = "info.febvre.Komikku";
+      type = "Application";
+      categories = [ "Graphics" "Network" "Viewer" ];
+      keywords = [ "manga" "reader" "viewer" "comic" "manhwa" "manhua" "bd" "webtoon" "webcomic" "scan" "offline" "GTK" "GNOME" ];
+      comment = "Access Komikku via proxychains";
+      startupWMClass = "Komikku";
+    })
   ];
 }
